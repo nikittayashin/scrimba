05fdd62d16dac61140dbd447e4969bf21334ec01
nikittayashin <nikita@resultdevelopment.com>
2022-05-02 11:47:31 +0200

    WIP

---
+++ index.js
@@ -0,0 +1,5 @@
+let meetersToFeet = document.getElementById('meetersToFeet');
+let litersToGallons = document.getElementById('litersToGallons');
+let kilosToPounds = document.getElementById('kilosToPounds')
+
+function 
